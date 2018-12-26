7357e61151c047e28c6392a945c1487bc4fb0ddf
tomitri <tomitri@yahoo.fr>
2018-12-26 23:02:28 +0100

    Update CustomerAccount.java

--- src/main/java/com/priceminister/account/implementation/CustomerAccount.java
+++ src/main/java/com/priceminister/account/implementation/CustomerAccount.java
@@ -4,14 +4,30 @@ import com.priceminister.account.*;
 
 
 public class CustomerAccount implements Account {
+    
+    private Double amountAccount;
+    
+    public CustomerAccount() {
+        this.amountAccount = 0.0;
+    }
+    
+    public void setAmountAccount(Double amount) {
+        this.amountAccount = amount;
+    }
+    
+    public Double getAmountAccount() {
+        // TODO Auto-generated method stub
+        return this.amountAccount;
+    }
 
     public void add(Double addedAmount) {
         // TODO Auto-generated method stub
+        this.amountAccount += addedAmount;
     }
 
     public Double getBalance() {
         // TODO Auto-generated method stub
-        return null;
+        return getAmountAccount();
     }
 
     public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule) 
